b69fec7e41db3b476da212cf12780fff0aa1a90a
Peter Huppertz <peter@dnl-core.net>
2019-09-24 11:34:54 +0200

    Removed TODO comments from already finished tasks

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -23,8 +23,6 @@ public class JobController {
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
-
-        // TODO #1 - get the Job with the given ID and pass it into the view
         Job job = jobData.findById(id);
         model.addAttribute(job);
         return "job-detail";
